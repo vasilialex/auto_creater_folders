edb117907d7e00e8494975d790307f725ae7a500
vasiliy <vasilli@bk.ru>
2022-05-31 11:28:00 +0600

    ver 1.0

--- autocreater.py
+++ autocreater.py
@@ -6,10 +6,10 @@ import os
 arguments = len(sys.argv)
 
 if arguments <= 1:
-    print("Please, provide directorys names separated by space")
+    print("Please, provide directories names separated by space")
     sys.exit()
 if sys.argv[1] == "--help":
-    print("I will create new directory. Please, provide directory names separated by space")
+    print("I will create new directories. Please, provide directories names separated by space")
     exit()
 if arguments > 1:
     for arg in sys.argv[1:]:
